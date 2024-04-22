c9c1f95c0e012f12bd00b3dde2ee77df23a3d751
lennythepenny <lena.sarieva@gmail.com>
2024-04-22 15:32:28 -0600

    added script.js

--- src/App.js
+++ src/App.js
@@ -95,6 +95,7 @@
 
 // export default App; 
 import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import RecipeCard from './RecipeCard';
 import './style.css';
 
@@ -103,31 +104,31 @@ const App = () => {
     const [searchRecipe, setSearchRecipe] = useState('');
     const [searchQuery, setSearchQuery] = useState('chicken');
 
-    const getRecipesFunction = async () => {
-        try {
-            const response = await fetch(
-                `${process.env.REACT_APP_API_URL}/search?q=${searchQuery}`
-            );
-            if (response.ok) {
-                const data = await response.json();
-                setFoodRecipes(data.hits);
-            } else {
-                console.error('Error fetching recipes:', response.statusText);
-            }
-        } catch (error) {
-            console.error('Error fetching recipes:', error);
-        }
-    };
-
     useEffect(() => {
-        getRecipesFunction();
-    }, [getRecipesFunction, searchQuery]);
+        const fetchRecipes = async () => {
+            try {
+                const response = await fetch(
+                    `${process.env.REACT_APP_API_URL}/search?q=${searchQuery}`
+                );
+                if (response.ok) {
+                    const data = await response.json();
+                    setFoodRecipes(data.hits);
+                } else {
+                    console.error('Error fetching recipes:', response.statusText);
+                }
+            } catch (error) {
+                console.error('Error fetching recipes:', error);
+            }
+        };
 
-    const updateSearchFunction = (e) => {
+        fetchRecipes();
+    }, [searchQuery]);
+
+    const handleSearchChange = (e) => {
         setSearchRecipe(e.target.value);
     };
 
-    const getSearchFunction = (e) => {
+    const handleSearchSubmit = (e) => {
         e.preventDefault();
         setSearchQuery(searchRecipe);
         setSearchRecipe('');
@@ -144,7 +145,7 @@ const App = () => {
             </header>
             <div className="container mx-auto mt-8 p-4 sm:px-6 lg:px-8">
                 <form
-                    onSubmit={getSearchFunction}
+                    onSubmit={handleSearchSubmit}
                     className="bg-white p-4 sm:p-6 lg:p-8 rounded-lg shadow-md flex flex-col sm:flex-row items-center justify-center space-y-4 sm:space-y-0 sm:space-x-4"
                 >
                     <div className="relative justify-center flex-grow w-full sm:w-1/2">
@@ -152,7 +153,7 @@ const App = () => {
                             type="text"
                             name="search"
                             value={searchRecipe}
-                            onChange={updateSearchFunction}
+                            onChange={handleSearchChange}
                             placeholder="Search for recipes..."
                             className="w-full py-3 px-4 bg-gray-100 border border-blue-300 focus:ring-blue-500 focus:border-blue-500 rounded-full text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:ring-2 focus:ring-blue-900 focus:bg-transparent focus:shadow-md"
                         />
@@ -169,10 +170,7 @@ const App = () => {
             <div className="container mx-auto mt-8 p-4 sm:px-6 lg:px-8">
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                     {foodRecipes.map((recipe) => (
-                        <RecipeCard
-                            key={recipe.recipe.label}
-                            recipe={recipe.recipe}
-                        />
+                        <RecipeCard key={recipe.recipe.label} recipe={recipe.recipe} />
                     ))}
                 </div>
             </div>
