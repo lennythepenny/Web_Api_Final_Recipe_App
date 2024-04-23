e917c95b9d6a1cef0b02bea0ae50e3ddd63f38eb
lennythepenny <lena.sarieva@gmail.com>
2024-04-22 19:38:07 -0600

    try and center the recipes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import './style.css';
 import './App.css';
 import App from './App';
 
@@ -11,5 +10,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-// If you want to start measuring performance in your app, pass a function
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
